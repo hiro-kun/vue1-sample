5c1a04a2837222547e6d5e41e82127d54a2b2ab0
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-06-23 17:02:37 +0900

    fix コンポーネント名を修正

--- member-app/src/router/index.js
+++ member-app/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
-import Sample from '@/components/User/User';
+import User from '@/components/User/User';
 
 Vue.use(Router);
 
@@ -15,7 +15,7 @@ export default new Router({
     {
       path: '/user/:id',
       name: 'User',
-      component: Sample,
+      component: User,
     },
   ],
 });
