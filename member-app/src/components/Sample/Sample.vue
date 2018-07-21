c051c53fa9929cbbc9b64c4e1d410adfa1ae3429
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-07-21 15:36:38 +0900

    add vuexのサンプルを追加

--- member-app/src/components/Sample/Sample.vue
+++ member-app/src/components/Sample/Sample.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <button @click="sample">API Kick</button>
+    <button @click="update">更新</button>
+    <button @click="repeat">アクションダミー</button>
 
     <h1>{{msg}}</h1>
+    <h1>{{count}}</h1>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import { mapGetters, mapState } from 'vuex';
 
 export default {
   data() {
@@ -16,11 +20,22 @@ export default {
     };
   },
   computed: {
-    msg() {
-      return this.$store.state.msg;
-    },
+    // 値の参照
+    ...mapState({
+      msg: state => state.msg,
+    }),
+    // storeに定義された関数を呼び出し
+    ...mapGetters([
+      'count',
+    ])
   },
   methods: {
+    update() {
+      this.$store.commit('updateMsg', 'sample');
+    },
+    repeat() {
+      this.$store.dispatch('apicall')
+    },
     sample() {
       axios.get('http://192.168.33.88/test.php')
         .then((response) => {
--- member-app/src/vuex/store.js
+++ member-app/src/vuex/store.js
@@ -6,7 +6,24 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     msg: 'Hello Vuex Store.',
+    count: 'testdesu',
   },
+  getters: {
+    count() {
+      return store.state.count;
+    },
+  },
+  mutations: {
+    updateMsg(state, newMsg) {
+      state.msg = newMsg;
+    },
+  },
+  actions: {
+    apicall(context) {
+      console.log("api-call");
+    },
+  }
+
 });
 
 export default store;
